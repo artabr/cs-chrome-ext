49453033611dc6b847bea97a0c97f64c067ea822
Artem Abramov <Artem_Abramov1@epam.com>
2023-01-29 16:05:34 +0300

    feat: initial split implementation

--- src/pages/content/components/Demo/index.tsx
+++ src/pages/content/components/Demo/index.tsx
@@ -1,11 +1,29 @@
-import { createRoot } from "react-dom/client";
-import App from "@src/pages/content/components/Demo/app";
 import refreshOnUpdate from "virtual:reload-on-update-in-view";
+import Split from 'split.js'
 
 refreshOnUpdate("pages/content/components/Demo");
 
-const root = document.createElement("div");
-root.id = "chrome-extension-boilerplate-react-vite-content-view-root";
-document.body.append(root);
+const leftSidebarSize = 25;
 
-createRoot(root).render(<App />);
+function onDragEnd(sizes: number[]) {
+    console.log("Split sizes:", sizes);
+}
+
+const observer = new MutationObserver(function (mutations_list) {
+    mutations_list.forEach(function (mutation) {
+        mutation.addedNodes.forEach(function (added_node) {
+            if (added_node.nodeType === Node.ELEMENT_NODE) {
+                const leftSidebarElement = (added_node as Element).getElementsByClassName("PageLayout__leftSidebar");
+                if (leftSidebarElement.length !== 0) {
+                    Split(['.PageLayout__leftSidebar', '.PageLayout__content'], {
+                        sizes: [leftSidebarSize, 100 - leftSidebarSize],
+                        onDragEnd
+                    })
+                    observer.disconnect();
+                }
+            }
+        });
+    });
+});
+
+observer.observe(document, {subtree: true, childList: true});
